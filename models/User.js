0cf369815d9aa4f7f3b2fabc623189d9b1d51680
Joseph Darville <joseph.darville9@gmail.com>
2022-07-17 23:14:47 -0400

    hashed our mf passwords

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
@@ -7,53 +8,52 @@ class User extends Model {}
 // define table columns and their types
 User.init(
   {
-    // define an id column
     id: {
-      // type of data is a number
       type: DataTypes.INTEGER,
-      // equivalent to NOT NULL
       allowNull: false,
-      // instruct that this is the primary key
       primaryKey: true,
-      // auto incrementing
       autoIncrement: true,
     },
-    // define a username column
     username: {
       type: DataTypes.STRING,
       allowNull: false,
     },
     email: {
       type: DataTypes.STRING,
-      // cannot duplicate email values in this table
+      allowNull: false,
       unique: true,
-      // if allowNull is false, we can run data through validor before creating table data
       validate: {
         isEmail: true,
       },
     },
-    // define a password column
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        // password must be at least 4 characters long
         len: [4],
       },
     },
   },
   {
-    // TABLE CONFIGURATION OPTION GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
-    // pass in our imported sequelize connection (the direct connection to our database)
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
     sequelize,
-    // don't automatically create createAt/updateAt timestamp fields
     timestamps: false,
-    // don't pluralize name of database table
     freezeTableName: true,
-    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
     underscored: true,
-    // make it so our model name stays lowercase in the database
     modelName: "user",
   }
 );
